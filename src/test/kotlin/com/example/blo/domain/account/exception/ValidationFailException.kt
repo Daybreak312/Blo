8bef64fe280c59d2a2336f0e42e42fcff6cc5756
Daybreak312 <ty82afg12@dsm.hs.kr>
2023-10-29 21:18:35 +0900

    !!🔥 :: 안 쓰이는 Exception 제거

--- src/test/kotlin/com/example/blo/domain/account/exception/ValidationFailException.kt
+++
@@ -1,6 +0,0 @@
-package com.example.blo.domain.account.exception
-
-import com.example.blo.global.error.CustomException
-import com.example.blo.global.error.ErrorCode
-
-object ValidationFailException : CustomException(ErrorCode.VALIDATION_FAIL)
