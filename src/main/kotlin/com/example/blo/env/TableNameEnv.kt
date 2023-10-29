508c788e6a134fcb276dce7e34eefa25d617e931
Daybreak312 <ty82afg12@dsm.hs.kr>
2023-10-29 21:02:57 +0900

    🐛 :: 대소문자 수정

--- src/main/kotlin/com/example/blo/env/TableNameEnv.kt
+++ src/main/kotlin/com/example/blo/env/TableNameEnv.kt
@@ -1,6 +1,6 @@
 package com.example.blo.env
 
 object TableNameEnv {
-    private const val tablePrefix = "TBL_"
-    const val accountTable = "${tablePrefix}ACCOUNT"
+    private const val tablePrefix = "tbl_"
+    const val accountTable = "${tablePrefix}account"
 }
