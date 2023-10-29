7d21b056223bd9e617937489ceb7c6102408acb7
Daybreak312 <ty82afg12@dsm.hs.kr>
2023-10-29 20:14:59 +0900

    🐛 :: loadUserByUsername에서 유저를 찾을 수 없을 때 일어나는 버그 수정

--- src/main/kotlin/com/example/blo/domain/account/service/LoadAccountService.kt
+++ src/main/kotlin/com/example/blo/domain/account/service/LoadAccountService.kt
@@ -1,13 +1,13 @@
 package com.example.blo.domain.account.service
 
+import com.example.blo.domain.account.entity.Account
 import com.example.blo.domain.account.persistence.AccountRepository
 import com.example.blo.domain.account.service.exception.AccountNotFoundException
-import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Component
 
 @Component
 class LoadAccountService(private val accountRepository: AccountRepository) : UserDetailsService {
-    override fun loadUserByUsername(username: String?): UserDetails =
-        username?.let { accountRepository.findByAccountId(it) as UserDetails } ?: throw AccountNotFoundException
+    override fun loadUserByUsername(username: String?): Account =
+        username?.let { accountRepository.findByAccountId(it) } ?: throw AccountNotFoundException
 }
