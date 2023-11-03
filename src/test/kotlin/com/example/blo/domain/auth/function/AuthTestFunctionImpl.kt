9d7a5d1038578f9753ddeb90468b67782cba524c
Daybreak312 <ty82afg12@dsm.hs.kr>
2023-11-03 18:31:50 +0900

    🐛 :: 빈 등록

--- src/test/kotlin/com/example/blo/domain/auth/function/AuthTestFunctionImpl.kt
+++ src/test/kotlin/com/example/blo/domain/auth/function/AuthTestFunctionImpl.kt
@@ -3,7 +3,9 @@ package com.example.blo.domain.auth.function
 import com.example.blo.domain.auth.presentation.dto.request.LoginRequest
 import com.example.blo.domain.auth.presentation.dto.request.SignRequest
 import com.example.blo.env.AccountTestEnv
+import org.springframework.stereotype.Component
 
+@Component
 class AuthTestFunctionImpl : AuthTestFunction {
 
     override fun createSignRequest() =
