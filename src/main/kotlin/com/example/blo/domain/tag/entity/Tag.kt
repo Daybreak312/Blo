5716ab6a7d099fac63d60ba6c0934ac7ddc1a038
Daybreak312 <ty82afg12@dsm.hs.kr>
2023-11-03 18:59:07 +0900

    ✨ :: 태그 추가

---
+++ src/main/kotlin/com/example/blo/domain/tag/entity/Tag.kt
@@ -0,0 +1,26 @@
+package com.example.blo.domain.tag.entity
+
+import com.example.blo.domain.account.entity.Account
+import com.example.blo.env.TableNameEnv
+import javax.persistence.*
+
+@Entity
+data class Tag(
+    @Column(length = 30)
+    val name: String,
+
+    @ManyToOne
+    @JoinColumn(name = TableNameEnv.accountTable)
+    val firstUser: Account
+) {
+    var usedCount: Long = 0
+        protected set
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null
+
+    fun addUsedCount() {
+        this.usedCount++
+    }
+}
