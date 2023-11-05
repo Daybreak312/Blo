8c18cfb6bd8bb456a71d9f4a45045b4d7539a672
Daybreak312 <ty82afg12@dsm.hs.kr>
2023-11-05 23:19:39 +0900

    ♻ :: 하드코딩 수정

--- src/test/kotlin/com/example/blo/domain/account/functionClass/AccountTestFunctionImpl.kt
+++ src/test/kotlin/com/example/blo/domain/account/functionClass/AccountTestFunctionImpl.kt
@@ -24,7 +24,7 @@ class AccountTestFunctionImpl(
     }
 
     private fun deleteTestAccountInRepository() {
-        accountRepository.deleteByAccountId("tester")
+        accountRepository.deleteByAccountId(AccountTestEnv.ACCOUNT_ID)
     }
 
     private fun initializeSecurityContextAuthentication() {
