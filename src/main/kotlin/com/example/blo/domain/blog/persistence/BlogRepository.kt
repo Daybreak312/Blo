2b7fb55f1aa9a9b50cda1af8f7e355fa306ff875
Daybreak312 <ty82afg12@dsm.hs.kr>
2023-11-05 20:59:28 +0900

    ✨ :: blog 엔티티 추가

---
+++ src/main/kotlin/com/example/blo/domain/blog/persistence/BlogRepository.kt
@@ -0,0 +1,11 @@
+package com.example.blo.domain.blog.persistence
+
+import com.example.blo.domain.account.entity.Account
+import com.example.blo.domain.blog.entity.Blog
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface BlogRepository : JpaRepository<Blog, Long?> {
+    fun existsByName(name: String): Boolean
+    fun findAllByOpener(opener: Account): List<Blog>
+    fun deleteByName(name: String)
+}
