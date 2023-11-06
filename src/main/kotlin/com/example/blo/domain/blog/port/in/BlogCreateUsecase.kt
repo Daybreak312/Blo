cc17b017529a78ebf7baddb865ce286b22806f84
Daybreak312 <ty82afg12@dsm.hs.kr>
2023-11-06 17:04:22 +0900

    🐛 :: 임포트 제거

--- src/main/kotlin/com/example/blo/domain/blog/port/in/BlogCreateUsecase.kt
+++ src/main/kotlin/com/example/blo/domain/blog/port/in/BlogCreateUsecase.kt
@@ -1,9 +1,6 @@
 package com.example.blo.domain.blog.port.`in`
 
 import com.example.blo.domain.blog.presentation.dto.request.BlogCreateRequest
-import com.example.blo.domain.blog.presentation.dto.request.BlogIntroductionUpdateRequest
-import com.example.blo.domain.blog.presentation.dto.request.BlogNameUpdateRequest
-import com.example.blo.domain.blog.presentation.dto.request.BlogTagUpdateRequest
 
 interface BlogCreateUsecase {
     fun createBlog(request: BlogCreateRequest)
