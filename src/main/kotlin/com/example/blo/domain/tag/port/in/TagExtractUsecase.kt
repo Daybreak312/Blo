1a6d620c9e644960cd470e0a509982558b85813a
Daybreak312 <ty82afg12@dsm.hs.kr>
2023-11-06 17:26:10 +0900

    ♻ :: tagFinder -> tagExtractor

--- src/main/kotlin/com/example/blo/domain/tag/port/in/TagExtractUsecase.kt
+++ src/main/kotlin/com/example/blo/domain/tag/port/in/TagExtractUsecase.kt
@@ -3,6 +3,6 @@ package com.example.blo.domain.tag.port.`in`
 import com.example.blo.domain.blog.entity.Blog
 import com.example.blo.domain.tag.entity.Tag
 
-interface TagFindUsecase {
-    fun findTagsInBlog(blog: Blog): List<Tag>
+interface TagExtractUsecase {
+    fun extractTagsInBlog(blog: Blog): List<Tag>
 }
--- src/main/kotlin/com/example/blo/domain/tag/service/TagExtractInteractor.kt
+++ src/main/kotlin/com/example/blo/domain/tag/service/TagExtractInteractor.kt
@@ -3,14 +3,14 @@ package com.example.blo.domain.tag.service
 import com.example.blo.domain.blog.entity.Blog
 import com.example.blo.domain.blog.persistence.BlogTagJoinerRepository
 import com.example.blo.domain.tag.entity.Tag
-import com.example.blo.domain.tag.port.`in`.TagFindUsecase
+import com.example.blo.domain.tag.port.`in`.TagExtractUsecase
 import org.springframework.stereotype.Service
 
 @Service
-class TagFindInteractor(
+class TagExtractInteractor(
     private val blogTagJoinerRepository: BlogTagJoinerRepository
-) : TagFindUsecase {
+) : TagExtractUsecase {
 
-    override fun findTagsInBlog(blog: Blog): List<Tag> =
+    override fun extractTagsInBlog(blog: Blog): List<Tag> =
         blogTagJoinerRepository.findAllByBlog(blog).map { it.tag }
 }
