2cdadf9e726a930bfa12ecb653bee7428c8cb1ca
Daybreak312 <ty82afg12@dsm.hs.kr>
2023-11-06 09:09:42 +0900

    🌟 :: PasswordEncoderConfiguration 분리

---
+++ src/main/kotlin/com/example/blo/global/security/configuration/PasswordEncoderConfiguration.kt
@@ -0,0 +1,4 @@
+package com.example.blo.global.security.configuration
+
+class PasswordEncoderConfiguration {
+}
--- src/main/kotlin/com/example/blo/global/security/configuration/SecurityConfiguration.kt
+++ src/main/kotlin/com/example/blo/global/security/configuration/SecurityConfiguration.kt
@@ -6,8 +6,6 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.crypto.factory.PasswordEncoderFactories
-import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
@@ -15,10 +13,6 @@ import org.springframework.security.web.SecurityFilterChain
 class SecurityConfiguration(
     private val filterConfiguration: FilterConfiguration
 ) {
-
-    @Bean
-    fun getPasswordEncoder(): PasswordEncoder =
-        PasswordEncoderFactories.createDelegatingPasswordEncoder()
 
     @Bean
     protected fun config(httpSecurity: HttpSecurity): SecurityFilterChain =
