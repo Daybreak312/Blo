1c788b0ec2bcddbf6ffb2b6c39b85ab12217f83b
Daybreak312 <ty82afg12@dsm.hs.kr>
2023-11-06 09:10:23 +0900

    🐛 :: 커밋 누락

--- src/main/kotlin/com/example/blo/global/security/configuration/PasswordEncoderConfiguration.kt
+++ src/main/kotlin/com/example/blo/global/security/configuration/PasswordEncoderConfiguration.kt
@@ -1,4 +1,13 @@
 package com.example.blo.global.security.configuration
 
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+
+@Configuration
 class PasswordEncoderConfiguration {
+    @Bean
+    fun getPasswordEncoder(): PasswordEncoder =
+        BCryptPasswordEncoder()
 }
