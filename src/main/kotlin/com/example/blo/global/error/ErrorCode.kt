6d3be79620711060c7eb25cdb3284de03b81e1ec
Daybreak312 <ty82afg12@dsm.hs.kr>
2023-11-07 17:24:03 +0900

    🐛 :: 에러코드 추가

--- src/main/kotlin/com/example/blo/global/error/ErrorCode.kt
+++ src/main/kotlin/com/example/blo/global/error/ErrorCode.kt
@@ -18,5 +18,10 @@ enum class ErrorCode(
     // Token
     TOKEN_WITHOUT_PREFIX(HttpStatus.BAD_REQUEST, "토큰이 정해진 접두사를 가지고 있지 않습니다."),
     NOT_JWT_TOKEN(HttpStatus.BAD_REQUEST, "JWT 토큰이 아닙니다."),
-    EXPIRED_TOKEN(HttpStatus.FORBIDDEN, "만료된 토큰입니다.")
+    EXPIRED_TOKEN(HttpStatus.FORBIDDEN, "만료된 토큰입니다."),
+
+    // Blog
+    IN_USE_BLOG_NAME(HttpStatus.CONFLICT, "사용하려는 이름이 이미 사용중인 이름입니다."),
+    BLOG_NOT_FOUND(HttpStatus.NOT_FOUND, "찾을 수 없는 블로그입니다."),
+    BLOG_NO_PERMISSION(HttpStatus.FORBIDDEN, "블로그에 접근할 수 있는 권한이 없습니다.")
 }
