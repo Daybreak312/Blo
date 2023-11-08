3fb74fd313656bd68b51c4cbc825b408eadd0b4b
Daybreak312 <ty82afg12@dsm.hs.kr>
2023-11-08 17:13:15 +0900

    🐛 :: 커밋 누락

--- src/main/kotlin/com/example/blo/domain/blog/persistence/BlogTagJoinerRepository.kt
+++ src/main/kotlin/com/example/blo/domain/blog/persistence/BlogTagJoinerRepository.kt
@@ -2,8 +2,10 @@ package com.example.blo.domain.blog.persistence
 
 import com.example.blo.domain.blog.entity.Blog
 import com.example.blo.domain.blog.entity.BlogTagJoiner
+import com.example.blo.domain.tag.entity.Tag
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface BlogTagJoinerRepository : JpaRepository<BlogTagJoiner, Long?> {
     fun findAllByBlog(blog: Blog): List<BlogTagJoiner>
+    fun deleteByTag(tag: Tag)
 }
