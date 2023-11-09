0845355a73f184be47bc8ffefe454f893fac895a
Daybreak312 <ty82afg12@dsm.hs.kr>
2023-11-09 23:29:03 +0900

    🐛 :: 커밋 누락

--- src/main/kotlin/com/example/blo/env/AccountTestEnv.kt
+++ src/main/kotlin/com/example/blo/env/AccountTestEnv.kt
@@ -3,6 +3,7 @@ package com.example.blo.env
 object AccountTestEnv {
 
     const val NAME = "테스터ㅌㅅㅌ"
-    const val ACCOUNT_ID = "tester"
+    const val ACCOUNT_ID = "tester@"
+    const val ACCOUNT_ID_2 = "tester2@"
     const val PASSWORD = "password@"
 }
